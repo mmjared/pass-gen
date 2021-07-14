81b652dabc0cad5f6555cbba83bf2fb5130a9c18
mmjared <85400903+mmjared@users.noreply.github.com>
2021-07-13 23:20:28 -0600

    Add files via upload

---
+++ pass-gen/script.js
@@ -0,0 +1,56 @@
+var generateBtn = document.querySelector("#generate")
+var passwordLength = document.querySelector("#length")
+var caps = document.querySelector("#caps")
+var lower = document.querySelector("#lower")
+var number = document.querySelector("#number")
+
+var items = Array()
+
+var numbersArray = Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+var lowerArray = Array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+var capArray = Array("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+
+function writepassword() {
+	var password = generatePassword()
+	var passwordText = document.querySelector("#password")
+
+	passwordText.value = password
+}
+
+function generatePassword() {
+	if (!passwordLength.value) {
+  	return "Missing How many characters"
+  }
+  
+  if (passwordLength.value < 8) {
+  	return "needs to be between 8 and 128 characters"
+  }
+  
+  if (passwordLength.value > 128) {
+  	return "needs to be between 8 and 128 characters"
+  }
+  
+  if (number.checked) {
+    items.push.apply(items, numbersArray)
+  }
+  
+  if (lower.checked) {
+  	items.push.apply(items, lowerArray)
+  }
+  
+  if (caps.checked) {
+  	items.push.apply(items, capArray)
+  }
+  
+  
+  let holder = ""
+  
+  for (let i = 0; i < passwordLength.value; i++) {
+    var item = items[Math.floor(Math.random()*items.length)];
+  	holder = holder.concat(item);
+	}
+
+	return holder
+}
+
+generateBtn.addEventListener("click", writepassword);
