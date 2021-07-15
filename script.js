a178e5ea47d5c3f47e5d7803513abd63a213064b
jared <mmjared11@outlook.com>
2021-07-14 21:41:01 -0600

    added speical characters

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ var passwordLength = document.querySelector("#length")
 var caps = document.querySelector("#caps")
 var lower = document.querySelector("#lower")
 var number = document.querySelector("#number")
+var special = document.querySelector("#special")
 
 var items = Array()
 
 var numbersArray = Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
 var lowerArray = Array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
 var capArray = Array("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+var specialArray = Array("!", "@", "#", "$", "%", "^", "&", "*",)
 
 function writepassword() {
 	var password = generatePassword()
@@ -42,7 +44,11 @@ function generatePassword() {
   	items.push.apply(items, capArray)
   }
   
+  if (special.checked) {
+    items.push.apply(items,  specialArray)
+  } 
   
+
   let holder = ""
   
   for (let i = 0; i < passwordLength.value; i++) {
